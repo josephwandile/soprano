3dc1558a53ade2a1b4f7f6679a80711634c041b3
Joseph Kahn <josephkahn@college.harvard.edu>
2017-07-29 14:27:44 -0700

    getting started with facebook/create-react-app

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import '../node_modules/bulma/css/bulma.css';
+import '../node_modules/font-awesome/css/font-awesome.css';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+          To get started, edit <code>src/App.js</code> and save to reload.
+      </div>
+    );
+  }
+}
+
+export default App;
