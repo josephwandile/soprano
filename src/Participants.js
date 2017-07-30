544795d34023bc1a9295682a4330e1686dfaae10
Joseph Kahn <josephkahn@college.harvard.edu>
2017-07-30 01:51:24 -0700

    adding text so users dont have to think for themselves

--- src/Participants.js
+++ src/Participants.js
@@ -80,7 +80,7 @@ class Participants extends Component {
             }
           })
         )
-      }, 4000);
+      }, 25000);
     }
 
   }
@@ -123,27 +123,41 @@ class Participants extends Component {
     );
 
     return (
-      <div className="container custom-container">
-        <div className="field has-addons">
-          <div className="control">
-            <input
-              value={this.state.newEmail}
-              placeholder='example@gmail.com'
-              className="input"
-              type="text"
-              onChange={this.handleChange}
-            />
+      <div className="columns">
+        <div className="column is-5">
+          <div className="field has-addons">
+            <div className="control">
+              <input
+                value={this.state.newEmail}
+                placeholder='example@gmail.com'
+                className="input"
+                type="text"
+                onChange={this.handleChange}
+              />
+            </div>
+            <div className="control" onClick={this.handleSubmit}>
+              <a
+                disabled={this.state.newEmail.length === 0 ? true : false}
+                className="button is-info"
+                >
+                Add Participant
+              </a>
+            </div>
           </div>
-          <div className="control" onClick={this.handleSubmit}>
-            <a
-              disabled={this.state.newEmail.length == 0 ? true : false}
-              className="button is-info"
-              >
-              Add Participant
-            </a>
+          {participants}
+        </div>
+        <div className="column is-7">
+          <div className="content">
+            <h3>
+              Some text to help you train Soprano
+            </h3>
+            <h4>Generative Unadversarial Networks</h4>
+            <code>https://arxiv.org/abs/1703.02528</code>
+            <p>
+                While the costs of human violence have attracted a great deal of attention from the research community, the effects of the network-on-network (NoN) violence popularised by Generative Adversarial Networks have yet to be addressed. In this work, we quantify the financial, social, spiritual, cultural, grammatical and dermatological impact of this aggression and address the issue by proposing a more peaceful approach which we term Generative Unadversarial Networks (GUNs). Under this framework, we simultaneously train two models: a generator G that does its best to capture whichever data distribution it feels it can manage, and a motivator M that helps G to achieve its dream. Fighting is strictly verboten and both models evolve by learning to respect their differences. The framework is both theoretically and electrically grounded in game theory, and can be viewed as a winner-shares-all two-player game in which both players work as a team to achieve the best score. Experiments show that by working in harmony, the proposed model is able to claim both the moral and log-likelihood high ground. Our work builds on a rich history of carefully argued position-papers, published as anonymous YouTube comments, which prove that the optimal solution to NoN violence is more GUNs.
+            </p>
           </div>
         </div>
-        {participants}
       </div>
     );
   }
