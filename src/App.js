7df758475b4ef8f9bbc997c14c99fffa8e3e9170
Yasyf Mohamedali <yasyfm@gmail.com>
2017-07-30 01:48:02 -0700

    Add TranscriptMock..

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import Recorder from './Recorder.js';
 import Participants from './Participants.js';
 import Transcript from './Transcript.js';
+import TranscriptMock from './TranscriptMock.js';
 import 'whatwg-fetch';
 import update from 'immutability-helper';
 
@@ -12,209 +13,7 @@ class App extends Component {
     super(props);
 
     this.state = {
-      transcript: {},
-      testTranscript: {
-        "1": [
-          {
-            "final": false,
-            "last": null,
-            "new": true,
-            "speaker": "SPEAKER_0",
-            "transcript": [
-              "indicates"
-            ]
-          },
-          {
-            "final": false,
-            "last": 0,
-            "new": false,
-            "speaker": "SPEAKER_0",
-            "transcript": [
-              "whether"
-            ]
-          },
-          {
-            "final": false,
-            "last": 0,
-            "new": false,
-            "speaker": "SPEAKER_0",
-            "transcript": [
-              "the"
-            ]
-          },
-          {
-            "final": false,
-            "last": 0,
-            "new": false,
-            "speaker": "SPEAKER_0",
-            "transcript": [
-              "service"
-            ]
-          },
-          {
-            "final": false,
-            "last": 0,
-            "new": false,
-            "speaker": "SPEAKER_0",
-            "transcript": [
-              "is"
-            ]
-          },
-          {
-            "final": false,
-            "last": 0,
-            "new": false,
-            "speaker": "SPEAKER_0",
-            "transcript": [
-              "to"
-            ]
-          },
-          {
-            "final": false,
-            "last": 0,
-            "new": false,
-            "speaker": "SPEAKER_0",
-            "transcript": [
-              "return"
-            ]
-          },
-          {
-            "final": false,
-            "last": 0,
-            "new": true,
-            "speaker": "SPEAKER_1",
-            "transcript": [
-              "interim"
-            ]
-          },
-          {
-            "final": false,
-            "last": 1,
-            "new": false,
-            "speaker": "SPEAKER_1",
-            "transcript": [
-              "results"
-            ]
-          },
-          {
-            "final": false,
-            "last": 1,
-            "new": true,
-            "speaker": "SPEAKER_2",
-            "transcript": [
-              "if"
-            ]
-          },
-          {
-            "final": false,
-            "last": 2,
-            "new": false,
-            "speaker": "SPEAKER_2",
-            "transcript": [
-              "true"
-            ]
-          },
-          {
-            "final": false,
-            "last": 2,
-            "new": true,
-            "speaker": "SPEAKER_1",
-            "transcript": [
-              "interim"
-            ]
-          },
-          {
-            "final": false,
-            "last": 1,
-            "new": false,
-            "speaker": "SPEAKER_1",
-            "transcript": [
-              "results"
-            ]
-          },
-          {
-            "final": false,
-            "last": 1,
-            "new": false,
-            "speaker": "SPEAKER_1",
-            "transcript": [
-              "are"
-            ]
-          },
-          {
-            "final": false,
-            "last": 1,
-            "new": false,
-            "speaker": "SPEAKER_1",
-            "transcript": [
-              "returned"
-            ]
-          },
-          {
-            "final": false,
-            "last": 1,
-            "new": false,
-            "speaker": "SPEAKER_1",
-            "transcript": [
-              "as"
-            ]
-          },
-          {
-            "final": false,
-            "last": 1,
-            "new": false,
-            "speaker": "SPEAKER_1",
-            "transcript": [
-              "a"
-            ]
-          },
-          {
-            "final": false,
-            "last": 1,
-            "new": false,
-            "speaker": "SPEAKER_1",
-            "transcript": [
-              "stream"
-            ]
-          },
-          {
-            "final": false,
-            "last": 1,
-            "new": false,
-            "speaker": "SPEAKER_1",
-            "transcript": [
-              "of"
-            ]
-          },
-          {
-            "final": false,
-            "last": 1,
-            "new": false,
-            "speaker": "SPEAKER_1",
-            "transcript": [
-              "trace"
-            ]
-          },
-          {
-            "final": false,
-            "last": 1,
-            "new": false,
-            "speaker": "SPEAKER_1",
-            "transcript": [
-              "on"
-            ]
-          },
-          {
-            "final": true,
-            "last": 1,
-            "new": false,
-            "speaker": "SPEAKER_1",
-            "transcript": [
-              "objects"
-            ]
-          },
-        ]
-      }
+      transcript: TranscriptMock,
     };
   }
 
@@ -259,7 +58,7 @@ class App extends Component {
           <Recorder appendToTranscript={this.appendToTranscript}/>
         </div>
         <div className="section">
-          <Transcript transcript={this.state.testTranscript}/>
+          <Transcript transcript={this.state.transcript}/>
         </div>
       </div>
     );
